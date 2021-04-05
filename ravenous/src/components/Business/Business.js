36b998f48237a355c37d71ab1075fbb7fe4448d4
James Sawkins <james.sawkins@googlemail.com>
2021-04-05 20:24:28 +0100

    step 14-16: added HTML to the render method of the business class. Then addedd CSS and linked to the .js file

--- ravenous/src/components/Business/Business.js
+++ ravenous/src/components/Business/Business.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './business.css';
 
 const business = {
     imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
@@ -14,6 +15,23 @@ const business = {
 
 class Business extends React.Component {
     render() {
-        
+        <div className="Business">
+            <div className="image-container">
+                <img src={business.imageSrc} alt={business.name} />
+            </div>
+            <h2>MarginOtto Pizzeria</h2>
+            <div className="Business-information">
+                <div className="Business-address">
+                    <p>{business.address}</p>
+                    <p>{business.city}</p>
+                    <p>{business.state} {business.zipCode}</p>
+                </div>
+                <div className="Business-reviews">
+                <h3>{business.category}</h3>
+                <h3 className="rating">{business.rating} stars}</h3>
+                <p>{business.reviewCount} reviews</p>
+                </div>
+            </div>
+        </div>
     }
-}
+};
