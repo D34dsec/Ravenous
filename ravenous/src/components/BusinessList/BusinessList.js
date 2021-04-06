c8b8858e3f381cb93509204ff4b97c112ed875c8
James Sawkins <james.sawkins@googlemail.com>
2021-04-06 10:10:20 +0100

    Step 22-24: created BusinessList component class and added HTML to render method

--- ravenous/src/components/BusinessList/BusinessList.js
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -2,3 +2,17 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
+class BusinessList extends React.Component {
+    render() {
+        return (
+            <div class="BusinessList">
+                <Business /> 
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div>
+        )
+    };
+}
