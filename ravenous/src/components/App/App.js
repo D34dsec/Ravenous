d28f4b6a68c366ccf46369124ddd29434834593f
James Sawkins <james.sawkins@googlemail.com>
2021-04-06 11:09:28 +0100

    Step 45: Import BusinessList and SearchBar

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
 function App() {
   return (
     <div className="App">
